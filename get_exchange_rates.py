b5929be939732bb0315cf09260763964f1745738
franbede <benaventedelgado@live.com>
2020-02-16 20:42:23 +0100

    Code refactoring for currency configuration

--- get_exchange_rates.py
+++ get_exchange_rates.py
@@ -10,19 +10,29 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import settings
 
-# EUR / USD exchange rate from BCE
-r =requests.get('https://api.exchangeratesapi.io/history?start_at=2018-12-28&end_at=2018-12-29&symbols=USD')
+
+# Start and stop dates for date filtering
+start_date = '2018-12-28'
+stop_date = '2018-12-29'
+
+# FIAT currency to get pair (EUR / fiat_currency)
+fiat_currency = 'USD'
+
+# EUR / fiat_currency exchange rate from BCE
+r =requests.get(f'https://api.exchangeratesapi.io/history?start_at={start_date}&end_at={stop_date}&symbols={fiat_currency}')
 print(f'Code: {r.status_code}')
 print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
 print()
 
 
 # Crypto exchange rates from CoinMarketCap
+fiat_market_currency = 'USD'
+
 url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
   'limit':'1',
-  'convert':'USD'
+  'convert':f'{fiat_market_currency}'
 }
 headers = {
   'Accepts': 'application/json',
